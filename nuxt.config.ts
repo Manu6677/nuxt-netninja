a553a30495278d1aedeeea060c80df33fe09d51b
manupanjoria <manu.panjoria@bonamisoftware.com>
2023-12-04 11:47:15 +0530

    setup

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Nuxt Netninja",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
